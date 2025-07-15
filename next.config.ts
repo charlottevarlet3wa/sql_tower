e6ca3197cf9d7c04f805dc4ef7d28916f415c9a3
charlottevarlet3wa <c.varlet@3wa.fr>
2025-07-15 12:27:44 +0200

    extconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/sql_tower" : "",
+  assetPrefix: isProd ? "/sql_tower/" : "",
 };
 
 export default nextConfig;
