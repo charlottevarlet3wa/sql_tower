bd737b9726692b658772cbda5c3d5312d42da175
charlottevarlet3wa <c.varlet@3wa.fr>
2025-07-15 13:34:47 +0200

    next actions

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/sql_tower" : "",
-  assetPrefix: isProd ? "/sql_tower/" : "",
+  // basePath: isProd ? "/sql_tower" : "",
+  // assetPrefix: isProd ? "/sql_tower/" : "",
 };
 
 export default nextConfig;
